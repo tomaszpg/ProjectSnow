4fa658cd2a1c0d0a8efa02383c98ea3e0f250a4d
tomaszpg <tomaszkraczaj@onet.pl>
2015-05-13 00:33:04 +0200

    i jeszcze raz...

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -31,10 +31,11 @@ namespace ConsoleApplication2
             {
                 Random random = new Random();
                 double[] pozycja_startowa = new double[3];
-                pozycja_startowa[0] = random.Next(0, (int)radius) * Math.Pow(-1.0, (double)(random.Next(1, 2)));    //Losowanie współrzędnej x płatka z zakresu od 0 do R z losowym znakiem
+                int promien = random.Next(0, (int)radius);
+                pozycja_startowa[0] =  (double)promien* Math.Pow(-1.0, (double)(random.Next(1, 2)));    //Losowanie współrzędnej x płatka z zakresu od 0 do R z losowym znakiem
                 pozycja_startowa[1] = SceneHeight;
-                pozycja_startowa[2] = Math.Sqrt(Math.Pow((double)random.Next(0, (int)radius), 2.0) - Math.Pow(pozycja_startowa[0], 2.0));  //wyznaczanie trzeciej współrzędnej płatka (z równania  okręgu o promieniu losowanym z przedziału <r,R> i środku (0,0)) z losowym znakiem
-                platki[i]=new SnowFlake(i,pozycja_startowa,0.001, 0.00000312);
+                pozycja_startowa[2] = Math.Sqrt(Math.Pow((double)promien, 2.0) - Math.Pow(pozycja_startowa[0], 2.0));  //wyznaczanie trzeciej współrzędnej płatka (z równania  okręgu o promieniu losowanym z przedziału <r,R> i środku (0,0)) z losowym znakiem
+                platki[i]=new SnowFlake(i,pozycja_startowa,0.000001, 0.00000312);
             }
             double K = otoczenie.getC_coefficient() * platki[0].getSize() * otoczenie.getDensity() * 0.5;   //Liczmy współczynnik K niezbędny do obliczenia prędkości granicznej- takiej samej dla każdego płatka (zakładamy, że są identyczne)
             //Wyznaczenie pozycji pojedyńczego płatka (np. pierwszego):
@@ -42,6 +43,13 @@ namespace ConsoleApplication2
             System.Console.WriteLine((platki[0].getPosition())[0].ToString());
             System.Console.WriteLine((platki[1].getPosition())[0].ToString());
             System.Console.WriteLine((platki[2].getPosition())[0].ToString());
+            System.Console.WriteLine(K.ToString());
+            System.Console.WriteLine(platki[0].LimitedSpeed(platki[0].getMass(), otoczenie.getGravitation(), K)[1].ToString());
+            for (int i = 0; i < 10000; i++)
+            {
+                platki[0].NewPosition(PlayerPos, platki[0].LimitedSpeed(platki[0].getMass(), otoczenie.getGravitation(), K), time_step, otoczenie);
+                System.Console.WriteLine(platki[0].getPosition()[0].ToString() + "\t" + platki[0].getPosition()[1].ToString() + "\t"+platki[0].getPosition()[2].ToString());
+            }
             Console.ReadKey();
         }
     }
