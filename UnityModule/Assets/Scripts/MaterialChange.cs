8d238d83f69992b03ee4aa52d7d89b29ab68f7bf
rabarbar <szybonos@gmail.com>
2015-06-03 01:35:50 +0200

    zmieniająca się tekstura
    Tekstura zmienia się w ustalanym w skrypcie czasie.
    Nie jest to zbyt uniwersalne rozwiązanie, ale innymi sposobami nie za
    bardzo mi się udawało niestety.

---
+++ UnityModule/Assets/Scripts/MaterialChange.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaterialChange : MonoBehaviour {
+
+    float time = 0.0f;
+    public int duration = 80;
+    private bool snowing = false;
+    private Renderer renderer;
+	void Start ()
+	{
+        renderer = GetComponent<Renderer>();
+        renderer.enabled = true;
+
+	}
+	
+
+	void Update () {
+	    if (snowing)
+	    {
+	        time += Time.deltaTime/duration;
+	        renderer.material.SetFloat("_Cutoff", Mathf.Lerp(0, 1, time));
+	    }
+	}
+
+
+    public void ItsSnowing()
+    {
+        snowing = true;
+    }
+
+    public void ItsNotSnowing()
+    {
+        snowing = false;
+    }
+}
